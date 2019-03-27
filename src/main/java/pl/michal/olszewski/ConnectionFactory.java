d3659779eb1e483ef915abb080cae93dc6a3effb
olszewskimichal <olszewskimichal@outlook.com>
2019-03-27 19:07:56 +0100

    polaczenie z baza

---
+++ src/main/java/pl/michal/olszewski/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package pl.michal.olszewski;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/dbname",
+                "username",
+                "password");
+    }
+}
